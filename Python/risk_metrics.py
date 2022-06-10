71acef3ab69e9eee3b83f9d9ab3e1f53e38c2346
TazTornadoo <janfaulstich@t-online.de>
2022-06-10 17:57:02 +0200

    save current stand

--- Python/risk_metrics.py
+++ Python/risk_metrics.py
@@ -1,31 +1,24 @@
 import pandas as pd
 import numpy as np
+import statistics
 
 
 class Risk_Metrics():
 
-    def __init__(self, trades, ohlcv_data, risk_free_return):
+    def __init__(self, trades, ohlcv_data, risk_free_return, daily_granularity, stats):
         self.trades = trades 
         self.ohlcv_data = ohlcv_data
         self.risk_free_return = risk_free_return
-    
-    
-    def _std_dev(price_timeseries):
-        
-        n = len(price_timeseries)
-        
-        mean = sum(price_timeseries) / n
-        
-        deviations = sum([(x - mean)**2 for x in price_timeseries])
-        variance = deviations / (n - 1)
-        standard_deviation = variance**(1/2)
-        
-        return standard_deviation
-    
+        self.daily_granularity = daily_granularity
+        self.stats = stats
     
     def _annualize(self, trade_return):
         
-        annualized_return = trade_return ** 1/ (len(self.ohlcv_data) / (24 * 60 * 365))
+        if self.daily_granularity == False:
+            annualized_return = trade_return ** 1/ (len(self.ohlcv_data) / (24 * 60 *365))
+        
+        else:
+            annualized_return = trade_return
         
         return annualized_return 
     
@@ -43,18 +36,19 @@ class Risk_Metrics():
             
             else:
                 temporary = self.ohlcv_data[column_name][row['EntryBar']:row['ExitBar']].to_list()
-                short_returns = [2 - x for x in temporary]
+                short_returns = [x for x in temporary]#[2 - x for x in temporary]
 
                 portfolio_returns.extend(short_returns)
         
-        portfolio_returns = [x for x in portfolio_returns if isinstance(x, str) != True]
-    
-        s = Risk_Metrics._std_dev(portfolio_returns)
-    
-        portfolio_return = np.cumprod(self.trades["ReturnPct"] + 1)
+        if self.daily_granularity == True:
+            portfolio_returns = self.trades["ReturnPct"] + 1
         
-        annual_return = self._annualize(portfolio_return[-1:].item())
+        portfolio_returns = [x for x in portfolio_returns if isinstance(x, str) != True]
 
+        s = statistics.stdev(portfolio_returns)
+        
+        annual_return = self._annualize(self.stats["Return [%]"] / 100)
+        
         sharpe_ratio = (annual_return - self.risk_free_return) / s
 
         return sharpe_ratio
@@ -72,16 +66,20 @@ class Risk_Metrics():
             
             else:
                 temporary = self.ohlcv_data[column_name][row['EntryBar']:row['ExitBar']].to_list()
-                short_returns = [2 - x for x in temporary]
+                short_returns = [x for x in temporary]
 
                 portfolio_returns.extend(short_returns) 
+                
+        if self.daily_granularity == True:
+            portfolio_returns = self.trades["ReturnPct"] + 1
+            
         
-        downside_portfolio_returns = [x for x in portfolio_returns if x < 1]
+        downside_portfolio_returns = [x if x < 1 else 0 for x in portfolio_returns]
         # Calculate Standard Deviation
-        s = Risk_Metrics._std_dev(downside_portfolio_returns)
-        portfolio_return = np.cumprod(self.trades["ReturnPct"] + 1)
+
+        s = statistics.stdev(downside_portfolio_returns)
         
-        annual_return = self._annualize(portfolio_return[-1:].item())
+        annual_return = self._annualize(self.stats["Return [%]"] / 100)
         
         sortino_ratio = (annual_return - self.risk_free_return) / s
         
@@ -102,8 +100,11 @@ class Risk_Metrics():
     
     def calmar_ratio(self, trade_return):
         
-        annual_return = self._annualize(trade_return)
+        annual_return = self._annualize(self.stats["Return [%]"] / 100)
+        
         if self.max_drawdown() > 0:
-            calmar_ratio = (annual_return - self.risk_free_return) / self.max_drawdown()
+            calmar_ratio_ = (annual_return - self.risk_free_return) / self.max_drawdown()
 
-        return calmar_ratio
+            return calmar_ratio_
+        
+        return 0
